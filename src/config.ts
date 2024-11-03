b64d6b59016c2ca9f59fda985c7d3b35cb3effb4
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-11-03 19:06:05 +0530

    feat: Added config.ts file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const callback_url =
-    "https://rnevu-2405-201-2007-103a-e559-6211-c793-336d.a.free.pinggy.link/api/webhook/submission";
+    "https://rnezk-2405-201-2007-103a-8c54-e5e3-acfe-3eea.a.free.pinggy.link/api/webhook/submission";
+
 const judge0Url = "http://localhost:2358";
 
 export { callback_url, judge0Url };
